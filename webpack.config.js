fe84b1d9a0aaaabec71ace7a7b3241b0d36db58b
Jimmy Reichley <jimmyqpublik@gmail.com>
2019-10-22 23:43:12 -0400

    Run tsc compilation through webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
+const path = require('path');
+
 module.exports = {
-    entry: "./dist/public/main.js",
-    output: {
-        filename: "public/bundle.js"
+    entry: './src/public/main.ts',
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
     },
-    optimization: {
-        minimize: false
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist', 'public')
     }
-}
+};

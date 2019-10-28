e051c664b010859478fbc3fede08e7ec2bd847b7
Jason Byrne <jason.byrne@flocasts.com>
2019-10-27 20:58:17 -0400

    Merge in Jimmy's changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,38 @@
+const path = require('path');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
 module.exports = {
-    entry: "./dist/public/main.js",
-    output: {
-        filename: "public/bundle.js"
+    entry: './app/src/main.ts',
+    devtool: 'source-map',
+    plugins: [
+        new CopyWebpackPlugin([
+            'app/index.js',
+            {
+                from: 'app/public/**/*',
+                to: 'public',
+                // NOTE(jpr): this is hacky but we need to move to get rid of
+                // index.js before we get rid of this
+                transformPath(targetPath, _) {
+                    return targetPath.replace(/^public\/app\//i, '');
+                },
+            },
+        ]),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
     },
-    optimization: {
-        minimize: false
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'public/bundle.js',
+        path: path.resolve(__dirname, 'dist'),
     }
-}
+};

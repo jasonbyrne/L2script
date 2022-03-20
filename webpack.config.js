3adb5759b0d8d2b51e666201c9af6bbd76f783d9
Jason Byrne <jbyrne@echelonfit.com>
2022-03-20 16:32:40 -0400

    getting ready to add help

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,42 @@
-const path = require('path');
+const path = require("path");
 
-const CopyWebpackPlugin = require('copy-webpack-plugin')
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-    entry: path.resolve('app', 'src', 'main.ts'),
-    devtool: 'source-map',
-    plugins: [
-        new CopyWebpackPlugin([
-            path.join('app', 'index.html'),
-            {
-                from: path.join('app', 'assets', '**', '*'),
-                to: 'assets',
-                flatten: true,
-            },
-        ]),
+  entry: path.resolve("app", "src", "main.ts"),
+  devtool: "source-map",
+  plugins: [
+    new CopyWebpackPlugin([
+      path.join("app", "index.html"),
+      {
+        from: path.join("app", "assets", "**", "*"),
+        to: "assets",
+        flatten: true,
+      },
+      {
+        from: path.join("app", "help", "**", "*"),
+        to: "help",
+        flatten: true,
+      },
+    ]),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        port: 5000,
-    },
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    port: 5000,
+  },
 };
